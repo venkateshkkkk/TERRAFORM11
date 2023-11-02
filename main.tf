df5db5c880b8a9230e1559600622e65828f643ed
venkatesh <venkateshkasa996@gmail.com>
2023-11-02 12:34:45 +0530

    sindu

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "aws" {
  access_key = "AKIAXNJHMXFKLFXHFLGA"
  secret_key = "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"
 }
-Resource "aws_instances" "aws"{
+resource "aws_instances" "aws"{
  ami = "ami-01bc990364452ab3e"
  instance_type = "t2.micro"
  count = 2
