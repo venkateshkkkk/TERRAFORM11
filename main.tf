e49ccf3ac48b28fbf734f4d0ef94e4366f7413dd
venkateshkkkk <124691574+venkateshkkkk@users.noreply.github.com>
2023-11-03 18:48:45 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,10 @@
 provider "aws" {
- region = "us-east-1"
- access_key = "AKIAXNJHMXFKLFXHFLGA"
- secret_key = "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"
+ region = "us-east-2"
+ access_key = "AKIAXNJHMXFKOYNJJEEN"
+ secret_key = "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"
 }
 resource "aws_instances" "aws"{
- ami = "ami-01bc990364452ab3e"
+ ami = "ami-0e83be366243f524a"
  instance_type = "t2.micro"
  count = 2
 tags = {
